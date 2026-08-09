b8e072a895dae71fa9a733db1b972a7164f8b382
Ywatch15 <pathaksundram82@gmail.com>
2026-08-09 14:41:32 +0530

    feat: changed import to const require.

--- scripts/sanitize-cloud-refs.js
+++ scripts/sanitize-cloud-refs.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const commandsDir = path.resolve('content/commands');
 const allSlugs = new Set();
